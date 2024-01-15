fe23d039eed17e0dbd80e611cc8402749e223e91
akshay agnihotri <120724324+akshay-agnihotri@users.noreply.github.com>
2024-01-15 13:49:27 +0000

    dataTypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+
+"use strict"; // treat all js code as newer version 
+// alert( 3+3) // we are using node.js not browser 
+
+let name="akshay" // string
+let age=20 // number
+let isLoggedIn=false // boolean
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+
+// object
+console.log(typeof null);
+console.log(typeof undefined);
+
+
