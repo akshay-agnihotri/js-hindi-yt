aec2fca5ec92de450f113a113285b871a6eaf61a
akshay agnihotri <120724324+akshay-agnihotri@users.noreply.github.com>
2024-01-15 13:00:34 +0000

    learning about variables

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,34 @@
+// to declare a const there is one way.
+const accountId=14423566
+
+/*
+to declare a variable there are two ways.
+initially js has only "var" keyword to declare a variable.
+prefer not to use var
+bcz of issue in block scope and functional scope.
+*/
+
+let accountEmail="akshay@gmail.com"
+var accountPassword="akshay@1234"
+let accountstate;
+
+/*
+declaring varibales without using let,var and const is bad practise
+https://dev.to/zhiyueyi/variable-declaration-in-javascript-var-let-or-const-1789#:~:text=myVariable%20%3D%20'abc'%3B%20It's%20possible,have%20created%20a%20global%20variable.&text=The%20result%20shows%20test%20%2C%20which%20means%20it%20works!
+*/
+
+accountCity="jaipur"
+
+
+
+// ques-1:-it is imp to add semicolon in js
+// ans=>https://www.geeksforgeeks.org/semicolon-in-javascript/
+
+
+// accountId=2  (not allowed)
+// console.log(accountId);
+accountEmail="abs@gmail.com"
+accountPassword="123444"
+accountCity="Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
