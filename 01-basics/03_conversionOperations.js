018f6d27a9cc0ed01296e6a47e28a3c8c6c69b84
akshay agnihotri <120724324+akshay-agnihotri@users.noreply.github.com>
2024-01-17 18:56:15 +0000

    operations in js

--- 01-basics/03_conversionOperations.js
+++ 01-basics/03_conversionOperations.js
@@ -20,6 +20,53 @@
 // "akshay" => true
 // "" => false
   
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber);
+
+
+// ************************** operations *********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello "
+let str2 = "akshay"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // here string is first so everything will be converted into string // bad practise
+// console.log(1 + "2"); // bad practise
+// console.log("1"+2+2); // bad practise
+// console.log("1"+2-2); // bad practise
+// console.log(1+2+"2"); // bad practise
+
+
+// console.log(+true); // bad practise
+// console.log(+""); // bad practise
+
+let num1,num2,num3
+// num1=num2=num3=2+2 // bad practise
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+
+/*
+link to study
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+*/
+
+
+
+
+
