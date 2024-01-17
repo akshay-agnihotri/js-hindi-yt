4d5fb54d4dd380dd629a37b177cffadf4d79923a
akshay agnihotri <120724324+akshay-agnihotri@users.noreply.github.com>
2024-01-17 19:59:05 +0000

    comparison operators

---
+++ 01-basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2>1);
+// console.log(2<1);
+
+// console.log("2">1); // "2" == 2
+// console.log("02">1);  // "02" == 2
+
+
+// console.log(null > 0); // avoid
+// console.log(null == 0); // avoid
+// console.log(null >= 0); // avoid
+
+// console.log(undefined == 0); // avoid
+// console.log(undefined > 0); // avoid
+// console.log(undefined < 0); // avoid
+
+// === 
+// console.log("2"===2);
